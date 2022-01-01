ef21e331d199b898477ea7085ac2b2b065031b43
Rajan <rajanpdl89@gmail.com>
2022-01-01 11:24:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.bottom === "50px") {
+    x.style.bottom= "-500px"
+  } else {
+    x.style.display = "block";
+    x.style.bottom = "50px";
+  }
+ }
+ 
+ function myFunction2() {
+  var x = document.getElementById("myDIV2");
+  if (x.style.bottom === "150px") {
+    x.style.bottom= "-720px";
+  } else {
+    x.style.display = "block";
+    x.style.bottom = "150px";
+  }
+ } 
+ 
+ function myFunction3() {
+  var x = document.getElementById("myDIV3");
+  if (x.style.right === "10px") {
+    x.style.right= "-400px"
+  } else {
+    x.style.display = "block";
+    x.style.right = "10px";
+  }
+ 
+ }  
+ function myFunction4() {
+  var x = document.getElementById("myDIV4");
+  if (x.style.display === "block") {
+    x.style.display= "none";
+  } else {
+    x.style.display = "block";
+  }
+ 
+ } 
+
+ function myFunction5() {
+  var x = document.getElementById("myDIV5");
+  if (x.style.bottom === "50px") {
+    x.style.bottom= "1100px";
+  } else {
+    x.style.display = "block";
+    x.style.bottom = "50px";
+  }
+ }
+ function myFunction6() {
+  var x = document.getElementById("myDIV6");
+  if (x.style.bottom === "50px") {
+    x.style.bottom= "-100px";
+  } else {
+    x.style.display = "block";
+    x.style.bottom = "50px";
+  }
+ } 
